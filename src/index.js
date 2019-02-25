6325110ddabcf52a0c9b4f9273f41b3ebb04b86c
Voltis <v.agolli@live.com>
2019-02-25 12:54:00 +0100

    navigation and routing

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { store } from './store';
 import { Provider } from 'react-redux';
+import { Router, Route } from 'react-router-dom';
+import { history } from './store/history';
+import { ConnectedNavigation } from './components/Navigation';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <Router history={history}>
+    <Provider store={store}>
+      <ConnectedNavigation />
+      <div>
+        <Route exact path="/dashboard" render={() => <App />} />
+      </div>
+    </Provider>
+  </Router>,
   document.getElementById('root')
 );
